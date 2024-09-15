fd22f531d9666a00f3466e4d0381a1563dc31272
ModassirHaque <166945118+ModassirHaque@users.noreply.github.com>
2024-09-15 09:47:18 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import BmiCalculator from './BmiCalculator'
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='App'>
+      <BmiCalculator/>
+   </div>
+  );
+}
+
+export default App
